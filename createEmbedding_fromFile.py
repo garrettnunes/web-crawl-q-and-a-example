59c8b26b8c764335d5b33dabed20f9f7d4910f95
garrettnunes <48533521+garrettnunes@users.noreply.github.com>
2024-04-23 21:18:56 -0400

    all

--- createEmbedding_fromFile.py
+++ createEmbedding_fromFile.py
@@ -26,154 +26,149 @@ openai.api_key = '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'
 # Define root domain to crawl
 # domain = "openai.com"
 # full_url = "https://openai.com/"
-domain = "library.municode.com"
-# full_url = "https://library.municode.com/tn/knoxville/codes/code_of_ordinances"
-full_url = "https://library.municode.com/tn/knoxville/ordinances/code_of_ordinances?nodeId=1283759"
+domain = "fazer.tech"
+full_url = "https://fazer.tech/"
 
 # Create a class to parse the HTML and get the hyperlinks
-class HyperlinkParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        # Create a list to store the hyperlinks
-        self.hyperlinks = []
+# class HyperlinkParser(HTMLParser):
+#     def __init__(self):
+#         super().__init__()
+#         # Create a list to store the hyperlinks
+#         self.hyperlinks = []
 
-    # Override the HTMLParser's handle_starttag method to get the hyperlinks
-    def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
+#     # Override the HTMLParser's handle_starttag method to get the hyperlinks
+#     def handle_starttag(self, tag, attrs):
+#         attrs = dict(attrs)
 
-        # If the tag is an anchor tag and it has an href attribute, add the href attribute to the list of hyperlinks
-        if tag == "a" and "href" in attrs:
-            self.hyperlinks.append(attrs["href"])
+#         # If the tag is an anchor tag and it has an href attribute, add the href attribute to the list of hyperlinks
+#         if tag == "a" and "href" in attrs:
+#             self.hyperlinks.append(attrs["href"])
 
 ################################################################################
 ### Step 2
 ################################################################################
 
-# Function to get the hyperlinks from a URL
-def get_hyperlinks(url):
+# # Function to get the hyperlinks from a URL
+# def get_hyperlinks(url):
     
-    # Try to open the URL and read the HTML
-    try:
-        # Open the URL and read the HTML
-        with urllib.request.urlopen(url) as response:
+#     # Try to open the URL and read the HTML
+#     try:
+#         # Open the URL and read the HTML
+#         with urllib.request.urlopen(url) as response:
 
-            # If the response is not HTML, return an empty list
-            # print(response.info())
-            if not response.info().get('Content-Type').startswith("text/html"):
-                return []
+#             # If the response is not HTML, return an empty list
+#             if not response.info().get('Content-Type').startswith("text/html"):
+#                 return []
             
-            # Decode the HTML
-            html = response.read().decode('utf-8')
-            print(html)
-    except Exception as e:
-        print(e)
-        return []
+#             # Decode the HTML
+#             html = response.read().decode('utf-8')
+#     except Exception as e:
+#         print(e)
+#         return []
 
-    # Create the HTML Parser and then Parse the HTML to get hyperlinks
-    parser = HyperlinkParser()
-    parser.feed(html)
+#     # Create the HTML Parser and then Parse the HTML to get hyperlinks
+#     parser = HyperlinkParser()
+#     parser.feed(html)
 
-    return parser.hyperlinks
+#     return parser.hyperlinks
 
-################################################################################
-### Step 3
-################################################################################
+# ################################################################################
+# ### Step 3
+# ################################################################################
 
-# Function to get the hyperlinks from a URL that are within the same domain
-def get_domain_hyperlinks(local_domain, url):
-    clean_links = []
-    for link in set(get_hyperlinks(url)):
-        clean_link = None
+# # Function to get the hyperlinks from a URL that are within the same domain
+# def get_domain_hyperlinks(local_domain, url):
+#     clean_links = []
+#     for link in set(get_hyperlinks(url)):
+#         clean_link = None
 
-        print(link)
+#         # If the link is a URL, check if it is within the same domain
+#         if re.search(HTTP_URL_PATTERN, link):
+#             # Parse the URL and check if the domain is the same
+#             url_obj = urlparse(link)
+#             if url_obj.netloc == local_domain:
+#                 clean_link = link
 
-        # If the link is a URL, check if it is within the same domain
-        if re.search(HTTP_URL_PATTERN, link):
-            # Parse the URL and check if the domain is the same
-            url_obj = urlparse(link)
-            if url_obj.netloc == local_domain:
-                clean_link = link
+#         # If the link is not a URL, check if it is a relative link
+#         else:
+#             if link.startswith("/"):
+#                 link = link[1:]
+#             elif (
+#                 link.startswith("#")
+#                 or link.startswith("mailto:")
+#                 or link.startswith("tel:")
+#             ):
+#                 continue
+#             clean_link = "https://" + local_domain + "/" + link
 
-        # If the link is not a URL, check if it is a relative link
-        else:
-            if link.startswith("/"):
-                link = link[1:]
-            elif (
-                link.startswith("#")
-                or link.startswith("mailto:")
-                or link.startswith("tel:")
-            ):
-                continue
-            clean_link = "https://" + local_domain + "/" + link
+#         if clean_link is not None:
+#             if clean_link.endswith("/"):
+#                 clean_link = clean_link[:-1]
+#             clean_links.append(clean_link)
 
-        if clean_link is not None:
-            if clean_link.endswith("/"):
-                clean_link = clean_link[:-1]
-            clean_links.append(clean_link)
-
-    # Return the list of hyperlinks that are within the same domain
-    return list(set(clean_links))
+#     # Return the list of hyperlinks that are within the same domain
+#     return list(set(clean_links))
 
 
-################################################################################
-### Step 4
-################################################################################
+# ################################################################################
+# ### Step 4
+# ################################################################################
 
-def crawl(url):
-    # Parse the URL and get the domain
-    local_domain = urlparse(url).netloc
+# def crawl(url):
+#     # Parse the URL and get the domain
+#     local_domain = urlparse(url).netloc
 
-    # Create a queue to store the URLs to crawl
-    queue = deque([url])
+#     # Create a queue to store the URLs to crawl
+#     queue = deque([url])
 
-    # Create a set to store the URLs that have already been seen (no duplicates)
-    seen = set([url])
+#     # Create a set to store the URLs that have already been seen (no duplicates)
+#     seen = set([url])
 
-    # Create a directory to store the text files
-    if not os.path.exists("text/"):
-            os.mkdir("text/")
+#     # Create a directory to store the text files
+#     if not os.path.exists("text/"):
+#             os.mkdir("text/")
 
-    if not os.path.exists("text/"+local_domain+"/"):
-            os.mkdir("text/" + local_domain + "/")
+#     if not os.path.exists("text/"+local_domain+"/"):
+#             os.mkdir("text/" + local_domain + "/")
 
-    # Create a directory to store the csv files
-    if not os.path.exists("processed"):
-            os.mkdir("processed")
+#     # Create a directory to store the csv files
+#     if not os.path.exists("processed"):
+#             os.mkdir("processed")
 
-    # While the queue is not empty, continue crawling
-    while queue:
+#     # While the queue is not empty, continue crawling
+#     while queue:
 
-        # Get the next URL from the queue
-        url = queue.pop()
-        print(url) # for debugging and to see the progress
+#         # Get the next URL from the queue
+#         url = queue.pop()
+#         print(url) # for debugging and to see the progress
         
-        # Try extracting the text from the link, if failed proceed with the next item in the queue
-        try:
-            # Save text from the url to a <url>.txt file
-            with open('text/'+local_domain+'/'+url[8:].replace("/", "_").replace("?", "_") + ".txt", "w", encoding="UTF-8") as f:
+#         # Try extracting the text from the link, if failed proceed with the next item in the queue
+#         try:
+#             # Save text from the url to a <url>.txt file
+#             with open('text/'+local_domain+'/'+url[8:].replace("/", "_") + ".txt", "w", encoding="UTF-8") as f:
 
-                # Get the text from the URL using BeautifulSoup
-                soup = BeautifulSoup(requests.get(url).text, "html.parser")
+#                 # Get the text from the URL using BeautifulSoup
+#                 soup = BeautifulSoup(requests.get(url).text, "html.parser")
 
-                # Get the text but remove the tags
-                text = soup.get_text()
+#                 # Get the text but remove the tags
+#                 text = soup.get_text()
 
-                # If the crawler gets to a page that requires JavaScript, it will stop the crawl
-                if ("You need to enable JavaScript to run this app." in text):
-                    print("Unable to parse page " + url + " due to JavaScript being required")
+#                 # If the crawler gets to a page that requires JavaScript, it will stop the crawl
+#                 if ("You need to enable JavaScript to run this app." in text):
+#                     print("Unable to parse page " + url + " due to JavaScript being required")
             
-                # Otherwise, write the text to the file in the text directory
-                f.write(text)
-        except Exception as e:
-            print("Unable to parse page " + url + " " + str(e))
+#                 # Otherwise, write the text to the file in the text directory
+#                 f.write(text)
+#         except Exception as e:
+#             print("Unable to parse page " + url)
 
-        # Get the hyperlinks from the URL and add them to the queue
-        for link in get_domain_hyperlinks(local_domain, url):
-            if link not in seen:
-                queue.append(link)
-                seen.add(link)
+#         # Get the hyperlinks from the URL and add them to the queue
+#         for link in get_domain_hyperlinks(local_domain, url):
+#             if link not in seen:
+#                 queue.append(link)
+#                 seen.add(link)
 
-crawl(full_url)
+# crawl(full_url)
 
 ################################################################################
 ### Step 5
@@ -212,9 +207,9 @@ df['text'] = df.fname + ". " + remove_newlines(df.text)
 df.to_csv('processed/scraped.csv', escapechar='\\')
 df.head()
 
-################################################################################
-### Step 7
-################################################################################
+###############################################################################
+## Step 7
+###############################################################################
 
 # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
 tokenizer = tiktoken.get_encoding("cl100k_base")
@@ -371,6 +366,7 @@ def answer_question(
     """
     Answer a question based on the most similar context from the dataframe texts
     """
+    print(f"Question: {question}")
     context = create_context(
         question,
         df,
@@ -405,4 +401,4 @@ def answer_question(
 
 print(answer_question(df, question="What day is it?", debug=False))
 
-print(answer_question(df, question="What is our newest embeddings model?"))
+print(answer_question(df, question="What is fazer.tech's email?"))
